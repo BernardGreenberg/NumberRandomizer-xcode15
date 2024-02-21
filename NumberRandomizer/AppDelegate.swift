d90dc11c554ab03ffa9336285f1f7d9da99fd541
Bernard Greenberg <53483365+BernardGreenberg@users.noreply.github.com>
2024-02-21 07:52:47 -0500

    Update AppDelegate.swift
    Lengthy comment added to AppDelegate.swift, which is currently not even used in the build, but is carried as freight.

--- NumberRandomizer/AppDelegate.swift
+++ NumberRandomizer/AppDelegate.swift
@@ -8,7 +8,15 @@
 import Foundation
 import AppKit
 
-/* https://stackoverflow.com/questions/65743619/close-swiftui-application-when-last-window-is-closed */
+/* This is not currently used in the build -- the "Window vs WindowGroup" solution, creating
+a single-window App, works better for getting it to exit when the window is closed.  But including
+this, and 
+  @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+in the App structure, really does add this AppKit cliché to the SwiftUI app, and works, too (and
+allows multiple windows, fwiw.
+*/
+
+/*  Seehttps://stackoverflow.com/questions/65743619/close-swiftui-application-when-last-window-is-closed */
 
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
